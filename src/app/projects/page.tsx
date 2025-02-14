e92d66f66f239cb7bfbc028df87909df632449ab
Clint Animbom <clint360.rebase@gmail.com>
2025-02-14 22:16:33 +0100

    partially worked on the projects secion

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,4 @@
+import { BreadcrumbImage } from '@/core/components/molecules/BreadcrumbImage'
 import Footer from '@/core/components/organisms/Footer'
 import Navbar from '@/core/components/organisms/Navbar'
 import React from 'react'
@@ -6,6 +7,10 @@ function page() {
   return (
     <div>
         <Navbar />
+        <BreadcrumbImage title='Our Projects' subtitle='Feel free to reach out to us'/>
+        <div className='h-96 bg-white text-center text-grey-700' >
+            Coming soon
+        </div>
         <Footer />
     </div>
   )
