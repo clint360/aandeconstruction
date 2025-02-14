367e544013a39baee9d580061404723932ebfaee
Clint Animbom <clint360.rebase@gmail.com>
2025-02-14 21:30:09 +0100

    created breadcrumb image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,27 @@
+import { BreadcrumbImage } from '@/core/components/molecules/BreadcrumbImage'
 import Footer from '@/core/components/organisms/Footer'
 import Navbar from '@/core/components/organisms/Navbar'
 import React from 'react'
 
 function page() {
-  return (
-    <div>
-        <Navbar />
-        
-        <Footer />
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <BreadcrumbImage
+                    title="About Us"
+                    subtitle="Welcome to A & E Construction Company"
+                    backgroundImage="/path-to-your-image.jpg"
+                />
+                <div className="p-8">
+                    <p className="text-gray-700">
+                        At A & E Construction, we are a leading construction company based in Cameroon, dedicated to providing top-notch construction services and associated solutions. With years of industry experience, a team of skilled professionals, and a commitment to quality, we aim to deliver exceptional results that not only meet but exceed our clients' expectations.
+                    </p>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default page
